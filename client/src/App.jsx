b957dbcaeb6414823eaa96c1d9f2c95e6f6353ff
Pratham shetty <prathamshetty484@gmail.com>
2024-06-23 15:12:16 +0530

    register page ui done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Register from './Pages/Register.jsx'
+//import Login from './Pages/Login.jsx'
+//import Chat from './Pages/Chat.jsx'
 
 function App() {
   
 
   return (
     <>
+    <Routes>
+      <Route path="/" element={<Register/>}></Route>
+      {/*<Route path="/login" element={<Login/>}></Route>
+      <Route path="/chat" element={<Chat/>}></Route>*/}
+    
+    </Routes>
       
     </>
   )
---
+++ client/src/Pages/Register.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import {toast} from "react-hot-toast";
+import logo from '../assets/logo.svg'
+function Register(){
+
+    
+    const navigate=useNavigate();
+    
+    const [signupData,setSignupData]=useState({
+        username:"",
+        email:"",
+        password:"",
+        confirmPassword:"",
+    });
+    
+    function handleUserInput(e){
+        const {name,value}=e.target;
+        setSignupData({
+            ...signupData,
+            [name]:value
+        })
+    }
+    async function handleSubmit(event){
+        event.preventDefault();
+        if(!signupData.email || !signupData.password || !username.fullName || !signupData.confirmpassword){
+            toast.error("Please fill all details");
+            return;
+
+        }
+        //checking name field length
+        if(signupData.username.length<3){
+            toast.error("name should be atleast of 3 characters");
+            return;
+        }
+        //email validating
+        if(!signupData.email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+            toast.error("Invalid email id");
+            return;
+        }
+        //password validating
+        if(!signupData.password.match( /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)){
+            toast.error("password must be 6-16 character long with atleast one number and one special character")
+        }
+        
+       
+        
+        
+        //const response=await dispatch(createAccount(formData));
+        
+           
+
+        
+        setSignupData({
+                username:"",
+                email:"",
+                password:"",
+                confirmPassword:"",
+        });
+       
+        
+
+            }
+           
+            
+
+
+    
+    return ( <div className="h-[100vh] w-[100vw] bg-[#131324] flex items-center justify-center">
+    <form onSubmit={handleSubmit} className="bg-[#00000076] flex flex-col rounded-3xl px-[4rem] pt-[2.5rem] pb-[2rem] shadow-[0_0_5px_gray]">
+        <div className="flex">
+            <img src={logo} alt="hi" className="h-[50px] w-[50px] mr-[1rem]"></img>
+            <h1 className="font-bold text-white text-3xl">ExpressoGram</h1>
+        </div>
+        <input type="text" placeholder="Enter UserName" name="username" value={signupData.username} onChange={handleUserInput} className="bg-transparent p-[1rem] border-[0.1rem] border-[#4e0eff] rounded-2xl font-[1rem] text-white w-[100%] my-[1rem]"/>
+        <input type="email" placeholder="Enter Email" name="email" value={signupData.email} onChange={handleUserInput} className="bg-transparent p-[1rem] border-[0.1rem] border-[#4e0eff] rounded-2xl font-[1rem] text-white w-[100%] mb-[1rem]"/>
+        <input type="password" placeholder="Enter password" name="password" value={signupData.password} onChange={handleUserInput} className="bg-transparent p-[1rem] border-[0.1rem] border-[#4e0eff] rounded-2xl font-[1rem] text-white w-[100%] mb-[1rem]"/>
+        <input type="password" placeholder="Confirm password" name="ConfirmPassword" value={signupData.confirmPassword} onChange={handleUserInput} className="bg-transparent p-[1rem] border-[0.1rem] border-[#4e0eff] rounded-2xl font-[1rem] text-white w-[100%] mb-[1.5rem]"/>
+        <button type="submit" className="bg-[#997af0] hover:bg-[#4e0eff] p-[1rem] border-[0.1rem]  rounded-2xl font-[1rem] text-white w-[100%] mb-[0.5rem] ">Click</button>
+        <span className="text-white mx-[1rem]">Already have an account??   <Link to="/login" className="text-blue-500">Login</Link></span>
+    
+    
+    </form>
+
+    
+    
+    
+    
+    
+    
+    
+    </div>)}
+
+
+export default Register;
