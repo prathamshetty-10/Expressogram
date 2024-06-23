51e77891c4db182be5c8de68d8947550e1284b90
Pratham shetty <prathamshetty484@gmail.com>
2024-06-23 12:22:44 +0530

    completed the mini project

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+mongoose.set('strictQuery',false)
+const app=express();
+import config from 'dotenv'
+config.config();
+
+app.use(cors());
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URL)
+    .then((conn)=>{
+        console.log("connected to db:",conn.connection.host);
+    })
+    .catch((err)=>{
+        console.log(err.message);
+        process.exit();
+
+    })
+const server=app.listen(process.env.PORT,()=>{
+    console.log(`connected to server port ${process.env.PORT}`);
+})
