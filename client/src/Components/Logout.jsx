21726e3772bf33117eb585b8fbb286e74650767c
Pratham shetty <prathamshetty484@gmail.com>
2024-06-24 14:06:21 +0530

    adding chat window

--- client/src/Components/Logout.jsx
+++ client/src/Components/Logout.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import {BiPowerOff} from 'react-icons/bi'
 function Logout(){
+    const navigate=useNavigate();
+    const handleClick=async()=>{
+        localStorage.clear();
+        navigate("/");
+    }
     return(
-        <div>
-        
-        </div>
+        <button onClick={handleClick} className="flex items-center justify-center p-[0.6rem] bg-[#9a86f3] rounded-3xl text-3xl cursor-pointer hover:bg-[#ebe7ff] hover:text-blue-700">
+        <BiPowerOff/>
+        </button>
     )
 }
 export default Logout;
