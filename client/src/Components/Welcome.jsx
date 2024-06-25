67f25f47be2eef20850eec010ea1065455a5d25c
Pratham shetty <prathamshetty484@gmail.com>
2024-06-25 12:04:00 +0530

    final commit

--- client/src/Components/Welcome.jsx
+++ client/src/Components/Welcome.jsx
@@ -3,9 +3,9 @@ import robot from '../assets/robot.gif'
 function Welcome({currentuser}){
     return(
         <div className="flex flex-col items-center justify-center text-white text-3xl font-extrabold">
-        <img src={robot} className="mb-0 h-[450px] w-[450px]" />
-        <h1>Welcome, {currentuser.username}!!</h1>
-        <div className="">Please Select A Chat To Start Messaging.</div>
+        <img src={robot} className="mb-0 h-[350px] w-[350px] lg:h-[450px] lg:w-[450px]" />
+        <h1 className="mb-[40px]">Welcome, {currentuser.username}!!</h1>
+        <div className="flex justify-center items-center text-xl lg:text-2xl"><p>Please Select A Chat To Start Messaging.</p></div>
         
         
         </div>
